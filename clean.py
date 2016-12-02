f6dd96759849ae096f701ff6454bbb161019395e
kls602 <stephen.cirelli@capitalone.com>
2016-12-01 21:10:55 -0500

    cleaning up.

--- clean.py
+++ clean.py
@@ -1,37 +1,43 @@
 #!/bin/python
 import os
 from subprocess import call, check_output, CalledProcessError
+from collections import deque
 import shlex
 
-dupeFile = "/tmp/dupes.txt"
+DUPE_FILE = "/tmp/dupes.txt"
+STATE_FILE = "/tmp/dupesScanState.txt"
+BASE_PATH = "/Volumes/Public/Media/Games"
+SCAN_THESE_FOLDERS = ['Amiga',
+				'Atari_2600',
+				'Game_Gear',
+				'Gameboy',
+				'Gameboy_Advance',
+				'Gameboy_Color',
+				'MAME',
+				'NeoGeo_Pocket_Color',
+				'Neo_Geo',
+				'Nintendo_DS',
+				'Nintendo_NES',
+				'Sega32x',
+				'Sega_CD',
+				'Sega_Genesis_(Sega_Mega_Drive)',
+				'Sega_Master_System',
+				'Super_Nintendo',
+				'TurboGrafX16']
 
 def main():
-	# Open a file
-	basePath = "/Volumes/Public/Media/Games"
-	scanTheseFolders = ['Amiga',
-					'Atari_2600',
-					'Game_Gear',
-					'Gameboy',
-					'Gameboy_Advance',
-					'Gameboy_Color',
-					'MAME',
-					'NeoGeo_Pocket_Color',
-					'Neo_Geo',
-					'Nintendo_DS',
-					'Nintendo_NES',
-					'Sega32x',
-					'Sega_CD',
-					'Sega_Genesis_(Sega_Mega_Drive)',
-					'Sega_Master_System',
-					'Super_Nintendo',
-					'TurboGrafX16']
+	restoreStateAndScanFoldersForDupes()
 
-	filesInDir = os.listdir(basePath)
-	for folder in scanTheseFolders:
-		workingPath = basePath + '/' + folder + "/ROMs"
+def restoreStateAndScanFoldersForDupes(foldersToScan = SCAN_THESE_FOLDERS):
+	scanForDupes(restoreState(foldersToScan, getPersistedState()))
+
+def scanForDupes(foldersToScan):
+	for folder in foldersToScan:
+		workingPath = BASE_PATH + '/' + folder + "/ROMs"
 		if(os.path.exists(workingPath)):
+			persistState(folder)
 			listOfDups = getDedupeMap(workingPath)
-			writeToFile(listOfDups)
+			writeDupeListToFile(listOfDups)
 		else:
 			print('Does not exist', folder)
 
@@ -65,12 +71,44 @@ def getDedupeMap(folder):
 
 	return listOfDups
 
-def writeToFile(listOfDups):
+def writeDupeListToFile(listOfDups, dupeFile = DUPE_FILE):
 	# Open file
 	fd = os.open(dupeFile, os.O_RDWR | os.O_CREAT | os.O_APPEND)
 	# Writing text
 	ret = os.write(fd, ('\n'.join(listOfDups) + '\n').encode('utf-8'))
 	# Close opened file
 	os.close(fd)
+
+def persistState(currentFolder, stateFile = STATE_FILE):
+	if(os.path.exists(stateFile)):
+		os.remove(stateFile)
+
+	fd = os.open(stateFile, os.O_WR | os.O_CREAT)
+	ret = os.write(fd, (currentFolder).encode('utf-8'))
+	os.close(fd)
+
+def getPersistedState(stateFile = STATE_FILE):
+	ret = None
+	try:
+		txt = open(stateFile)
+		ret = txt.read()
+		txt.close()
+	except:
+		ret = None
+
+	return ret
+
+def restoreState(originalListOfFolders, lastFolderScanned):
+	lastFolderScanned = lastFolderScanned.strip()
+	if(lastFolderScanned):
+		q = deque(originalListOfFolders)	
+		for folder in originalListOfFolders:
+			if(lastFolderScanned.lower() != folder.lower()):
+				p = q.popleft()
+			else:
+				break;
+		return list(q)
+
+	return originalListOfFolders
 
 main()
